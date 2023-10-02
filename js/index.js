0f072f74e3597b7406919d79e7b59b414d40cb85
Arturo Valencia <10554086+arturovz23@users.noreply.github.com>
2023-10-01 22:33:09 -0700

    Se agrego la funcionalidad de cerrar menu al hacer clic en enlaces en formato tablet y mobile.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+const body = document.querySelector('body');
 const navToggle = document.querySelector('.nav__toggle');
 const navUl = document.querySelector('.nav__ul');
 
+
+//Agregar aria-label al icono de menu
 navToggle.addEventListener('click', () => {
     navUl.classList.toggle('nav__ul--visible');
 
@@ -10,3 +13,11 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-label', 'Abrir menu');
     }
 });
+
+// Funcion para cerrar menu al hacer clic en enlace en formato tablet - mobile
+const links = navUl.getElementsByTagName("a");
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function () {
+      navUl.classList.remove("nav__ul--visible");
+    });
+  }
